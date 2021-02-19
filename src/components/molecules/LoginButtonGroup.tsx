0df65cfb9f2caedf2f0e55f71ec12904626e7ecb
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-19 21:05:42 +0900

    [FE] test

--- src/components/molecules/LoginButtonGroup.tsx
+++ src/components/molecules/LoginButtonGroup.tsx
@@ -12,16 +12,15 @@ const LoginButtonGroup = (): JSX.Element => {
   const onLogin = (auth: string): (() => void) => {
     const login = (name: string) => () => {
       const url = setting.SERVER_ADDRESS + `/auth/${name}`;
-      fetch(url)
-        .then(res => {
-          window.location.href = res.url;
-        })
-        .then(res => {
-          console.log('res : ', res);
-          console.log('url : ', url);
-          console.log('cookie : ', document.cookie.split('=')[1]);
-          localStorage.setItem('cookie', document.cookie.split('=')[1]);
-        });
+      fetch(url).then(res => {
+        console.log(res);
+        console.log(res.headers);
+        console.log(url);
+        console.log(document.cookie.split('=')[1]);
+        localStorage.setItem('cookie', document.cookie.split('=')[1]);
+
+        window.location.href = res.url;
+      });
     };
     return login(auth);
   };
