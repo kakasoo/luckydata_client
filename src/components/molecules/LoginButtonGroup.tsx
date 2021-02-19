8fd5935bd73af66a7f1cdd36fd2f5c82f373e5c6
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-19 17:19:10 +0900

    test : github login test 용도의 console.log 추가

--- src/components/molecules/LoginButtonGroup.tsx
+++ src/components/molecules/LoginButtonGroup.tsx
@@ -3,12 +3,17 @@ import Button from '../atoms/Button';
 import setting from '../../config';
 
 const LoginButtonGroup = (): JSX.Element => {
-  const loginButtons = ['github', 'google', 'facebook'];
+  const loginButtons = [
+    'github',
+    'Unavailable. Plz wait.',
+    'Unavailable. Plz wait.',
+  ];
   const loginColor = ['rgb(70,70,70)', 'rgb(221,75,57)', 'rgb(59,89,152)'];
   const onLogin = (auth: string): (() => void) => {
     const login = (name: string) => () => {
       const url = setting.SERVER_ADDRESS + `/auth/${name}`;
       fetch(url).then(res => {
+        console.log(res);
         window.location.href = res.url;
         localStorage.setItem('cookie', document.cookie.split('=')[1]);
       });
