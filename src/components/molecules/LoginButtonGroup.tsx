1c9be79b0acad03c814aecb14de1430b63dc68fc
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-22 00:56:54 +0900

    [FE] feat : https://github.com/rmbh4211995/passport-react/blob/master/server/index.js

--- src/components/molecules/LoginButtonGroup.tsx
+++ src/components/molecules/LoginButtonGroup.tsx
@@ -11,16 +11,17 @@ const LoginButtonGroup = (): JSX.Element => {
   ];
   const loginColor = ['rgb(70,70,70)', 'rgb(221,75,57)', 'rgb(59,89,152)'];
   const onLogin = (auth: string): (() => void) => {
-    const login = (name: string) => () => {
+    const login = (name: string) => async () => {
       const url = setting.SERVER_ADDRESS + `/auth/${name}`;
-      fetch(url, {
+      const nextUrl = setting.SERVER_ADDRESS + `/auth`;
+      const result = await fetch(url, {
         cache: 'no-cache',
-      }).then(res => {
-        console.log('cookie : ', document.cookie.split('=')[1]);
-        localStorage.setItem('cookie', document.cookie.split('=')[1]);
-        console.log(15);
-        window.location.href = res.url + `?+c=${new Date()}`;
       });
+      window.location.href = result.url + `?+c=${new Date()}`;
+
+      const nextResult: any = await fetch(nextUrl);
+      console.log('cookie : ', nextResult);
+      localStorage.setItem('cookie', nextResult.token);
     };
     return login(auth);
   };
