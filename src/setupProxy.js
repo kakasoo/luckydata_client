f27e1237152cab84e64153e54ae7be69cbf1df9a
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-22 11:05:25 +0900

    [FE] fix : proxy target localhost to IP

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,24 +1,26 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const target = '1.237.141.37';
+console.log(target);
 module.exports = function (app) {
   app.use(
     '/auth',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target: 'http://1.237.141.37:4000',
       changeOrigin: true,
     }),
   );
   app.use(
     '/auth/github',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target: 'http://1.237.141.37:4000',
       changeOrigin: true,
     }),
   );
   app.use(
     '/auth/local',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target: 'http://1.237.141.37:4000',
       changeOrigin: true,
     }),
   );
