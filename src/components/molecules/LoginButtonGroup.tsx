ba72d01d48f643333f7b7360aabebabc103b93a6
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-24 14:20:06 +0900

    [FE] login 경로를 login 모달에 추가

--- src/components/molecules/LoginButtonGroup.tsx
+++ src/components/molecules/LoginButtonGroup.tsx
@@ -10,8 +10,14 @@ const LoginButtonGroup = (): JSX.Element => {
   const loginColor = ['rgb(70,70,70)', 'rgb(221,75,57)', 'rgb(59,89,152)'];
   const onLogin = (auth: string): (() => void) => {
     const login = (name: string) => async () => {
-      // TODO : oAuth button
-      name;
+      switch (name) {
+        case 'github':
+          location.href =
+            'https://github.com/login/oauth/authorize?client_id=Iv1.483937f604dec290&redirect_uri=http://www.luckydata.site/callback';
+          break;
+        default:
+          return;
+      }
     };
     return login(auth);
   };
